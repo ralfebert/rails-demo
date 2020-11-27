be1c5bc1a6c2dfaacf8c2202d031c02415c29199
Ralf Ebert <info@ralfebert.de>
2020-11-27 18:56:15 +0100

    ruby/rails updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_222911) do
+ActiveRecord::Schema.define(version: 2020_11_27_175059) do
 
   create_table "counters", force: :cascade do |t|
     t.string "name"
